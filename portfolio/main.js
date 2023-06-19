da0a69607192a431a43223245a6e9824ba8dd09b
mkk100 <tommychimes2003@gmail.com>
2023-06-19 10:22:55 -0700

    hmmm

--- portfolio/main.js
+++ portfolio/main.js
@@ -4,7 +4,7 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight,0.1,1000);
-const renderer = new THREE.WebGL1Renderer({
+const renderer = new THREE.WebGLRenderer({
   canvas: document.querySelector('#bg'),
 })
 renderer.setPixelRatio(window.devicePixelRatio);
@@ -15,7 +15,7 @@ camera.position.setX(-3);
 renderer.render(scene,camera);
 
 const geometry = new THREE.TorusGeometry(10,3,16,100)
-const material = new THREE.MeshStandardMaterial({color: 0xFF6347});
+const material = new THREE.MeshStandardMaterial({color: 0xff6347});
 const torus = new THREE.Mesh(geometry,material);
 scene.add(torus)
 
@@ -30,13 +30,6 @@ scene.add(pointLight, ambientLight);
 
 // const controls = new OrbitControls(camera, renderer.domElement);
 
-function animate(){
-  requestAnimationFrame(animate);
-  torus.rotation.x += 0.01;
-  torus.rotation.y += 0.005;
-  torus.rotation.z += 0.01;
-  renderer.render(scene,camera);
-}
 function addStar(){
   const geometry = new THREE.SphereGeometry(0.25, 24, 24);
   const material = new THREE.MeshStandardMaterial({ color: 0xffffff });
@@ -49,14 +42,44 @@ function addStar(){
   star.position.set(x, y, z);
   scene.add(star);
 }
+Array(200).fill().forEach(addStar);
+
+const spaceTexture = new THREE.TextureLoader().load('space.jpeg');
+scene.background = spaceTexture;
+
+const thomasTexture = new THREE.TextureLoader().load('thomas.jpg');
+
+const thomas = new THREE.Mesh(new THREE.BoxGeometry(3, 3, 3), new THREE.MeshBasicMaterial({ map: thomasTexture }));
+
+scene.add(thomas);
+
+
+const saturnTexture = new THREE.TextureLoader().load('saturn.jpeg');
+const normalTexture = new THREE.TextureLoader().load('normal.jpg');
+
+const saturn = new THREE.Mesh(
+  new THREE.SphereGeometry(3, 32, 32),
+  new THREE.MeshStandardMaterial({
+    map: saturnTexture,
+    normalMap: normalTexture,
+  })
+);
+scene.add(s);
+
+saturn.position.z = 30;
+saturn.position.setX(-10);
+
+thomas.position.z = -5;
+thomas.position.x = 2;
+
 function moveCamera() {
   const t = document.body.getBoundingClientRect().top;
-  moon.rotation.x += 0.05;
-  moon.rotation.y += 0.075;
-  moon.rotation.z += 0.05;
+  saturn.rotation.x += 0.05;
+  saturn.rotation.y += 0.075;
+  saturn.rotation.z += 0.05;
 
-  jeff.rotation.y += 0.01;
-  jeff.rotation.z += 0.01;
+  thomas.rotation.y += 0.01;
+  thomas.rotation.z += 0.01;
 
   camera.position.z = t * -0.01;
   camera.position.x = t * -0.0002;
@@ -64,6 +87,12 @@ function moveCamera() {
 }
 document.body.onscroll = moveCamera;
 moveCamera();
+function animate(){
+  requestAnimationFrame(animate);
+  torus.rotation.x += 0.01;
+  torus.rotation.y += 0.005;
+  torus.rotation.z += 0.01;
 
-Array(200).fill().forEach(addStar);
+  renderer.render(scene,camera);
+}
 animate()

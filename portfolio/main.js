2ed22398b533657a84897f9eaafe5afff029ab53
mkk100 <tommychimes2003@gmail.com>
2023-06-19 09:49:27 -0700

    base

---
+++ portfolio/main.js
@@ -0,0 +1,14 @@
+import './style.css'
+import javascriptLogo from './javascript.svg'
+import viteLogo from '/vite.svg'
+import { setupCounter } from './counter.js'
+import * as THREE from 'three';
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight,0.1,1000);
+const renderer = new THREE.WebGL1Renderer({
+  canvas: document.querySelector('#bg'),
+})
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setSize(window.innerWidth, window.innerHeight);
+camera.position.setZ(30);
